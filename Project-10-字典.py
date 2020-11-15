58b6d12ff82bcff97d50e2d42665c2c0c0d9225c
Sourlun <1141837289@qq.com>
2020-11-15 23:10:21 +0800

    添加 字典.py

---
+++ Project-10-字典.py
@@ -0,0 +1,12 @@
+v1 = {'aa':'啊啊', 'bb':'版本', 'cc':'出错', 'dd':'读多'}
+print(v1)
+print(v1['bb'])
+
+# 成为映射关系
+dict1 = dict([(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd')])
+print(dict1)
+dict2 = dict(aa='啊啊啊', bb='不不不', cc='踩踩踩')
+print(dict2)
+# 添加映射
+dict2['dd'] = '点对点'
+print(dict2)
